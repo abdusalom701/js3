c308319bd082073334dca5e1963dae3b7048a447
abdusalom701 <ulugbekovabdusalom@gmail.com>
2024-08-21 18:25:00 +0500

    rshuygrqwerty

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt('ismingizni kiriting')
+
+let b = prompt('bironta harf kiriting')
+
+if('abdusalom'){
+    alert('Abdusalom ismining ichida s harfi mavjud')
+}else{
+    alert('Abdusalom ismining ichida q harfi mavjud')
+}
